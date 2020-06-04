1f1cb6290077d616fd070e2b064b03cf6cbafefa
Lakshman Sivakumar <lakshmansivakumar1298@gmail.com>
2020-06-04 19:12:58 +0530

    admin completed

--- routes/admin.js
+++ routes/admin.js
@@ -12,16 +12,54 @@ router.get('/login', function (req, res) {
     res.render('admin_login');
 });
 
-router.post('/course_change', function (req, res) {
-    console.log("post request of course_change")
-    console.log(req.body.courseChange)
-    /* var dbo = db.useDb("eggheads");
-    var myquery = { course: "ignite" };
-    var newvalues = { $set: { course: "kindle"} };
+router.post('/course_change/:id', function (req, res) {
+    //requiring the user's ID as in mongoDB
+    var id = req.params.id;
+    console.log(req.body);
+    var ObjectID = require('mongodb').ObjectID;
+
+    //requiring the changes made to the course and pay details
+    var courseChange = req.body.courseChange;
+    var Paid = parseInt(req.body.Paid);
+    console.log(courseChange, Paid);
+    //asking to use the 'eggheads' database
+    var dbo = db.useDb("eggheads");
+
+    //converting the user's ID to ObjectID
+    var myquery = { _id:  ObjectID(id)};
+
+    //setting the new values i.e the changed course name
+    var newvalues = { $set: { course: courseChange, paid: Paid} };
+
+    //updating to DB based on the query
     dbo.collection("users").updateOne(myquery, newvalues, function (err, res) {
         if (err) throw err;
         console.log("1 document updated");
-    }); */
+    });
+
+    //again displaying the user_data page with the UPDATED data
+    dbo.collection("users").find({}).toArray(function (err, result) {
+        if (err) throw err;
+        res.render('user_data', {
+            results: result,
+        });
+    });
+});
+
+router.post('/show_paid_user_data', function (req, res) {
+    //asking to use the 'eggheads' database
+    var dbo = db.useDb("eggheads");
+
+    //query the DB
+    var query = { paid: 1 };
+    dbo.collection("users").find(query).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.render('paid_user_data', {
+            results: result,
+        });
+        db.close();
+    });
 });
 
 router.post('/login', function (req, res) {
